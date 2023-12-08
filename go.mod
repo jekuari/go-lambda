5984726c8c7f2d29a92e713d14aca44749be4027
Ricardo Feregrino <ricardo@jekuari.mx>
2023-12-08 13:42:39 -0600

    Color generator

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.21.4
 require (
 	github.com/aws/aws-lambda-go v1.41.0
 	github.com/aws/aws-sdk-go v1.48.15
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 )
 
 require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- main.go
+++ main.go
@@ -1,33 +1,112 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
+type ColorEvent struct {
+	Color struct {
+		Hex string `json:"hex"`
+	} `json:"color"`
 }
 
-func HandleRequest(ctx context.Context, event *MyEvent) (*string, error) {
+func HandleRequest(ctx context.Context, event ColorEvent) (string, error) {
+	// Parse hex color to RGB
+	rgbColor, err := parseHexColor(event.Color.Hex)
+	if err != nil {
+		return "", err
+	}
+
+	// Create a 32x32 image with the specified color
+	img := createImage(rgbColor)
+
+	// Encode the image to PNG
+	var buf []byte
+	err = encodePNG(&buf, img)
+	if err != nil {
+		return "", err
+	}
+
+	// Upload the image to S3
+	err = uploadToS3(buf)
+	if err != nil {
+		return "", err
+	}
+
+	return "Image successfully generated and uploaded to S3", nil
+}
+
+func parseHexColor(hex string) (color.RGBA, error) {
+	// Parse hex color to RGB
+	var rgbColor color.RGBA
+	_, err := fmt.Sscanf(hex, "#%02x%02x%02x", &rgbColor.R, &rgbColor.G, &rgbColor.B)
+	if err != nil {
+		return color.RGBA{}, fmt.Errorf("failed to parse color: %v", err)
+	}
+	rgbColor.A = 255 // Alpha channel
+
+	return rgbColor, nil
+}
+
+func createImage(rgbColor color.RGBA) image.Image {
+	// Create a 32x32 image with the specified color
+	img := image.NewRGBA(image.Rect(0, 0, 32, 32))
+	draw.Draw(img, img.Bounds(), &image.Uniform{rgbColor}, image.Point{}, draw.Src)
+
+	return img
+}
+
+func encodePNG(buf *[]byte, img image.Image) error {
+	// Encode the image to PNG
+	err := png.Encode(os.Stdout, img)
+	if err != nil {
+		return fmt.Errorf("failed to encode image to PNG: %v", err)
+	}
+
+	return nil
+}
+
+func uploadToS3(buf []byte) error {
 	credentials := credentials.NewEnvCredentials()
 	sess, err := session.NewSession(&aws.Config{
 		Credentials: credentials,
 		Region:      aws.String("us-east-1"),
 	})
-
 	if err != nil {
-		fmt.Println(err)
+		return fmt.Errorf("failed to create AWS session: %v", err)
 	}
 
-	fmt.Println(sess)
+	imgRead := bytes.NewReader(buf)
 
-	return nil, nil
+	// Create an S3 client
+	s3Client := s3.New(sess)
+
+	// Upload the image to S3
+	_, err = s3Client.PutObject(&s3.PutObjectInput{
+		Bucket:      aws.String("colors"),
+		Key:         aws.String("generated_image.png"),
+		Body:        aws.ReadSeekCloser(imgRead),
+		ACL:         aws.String("public-read"), // Set appropriate ACL
+		ContentType: aws.String("image/png"),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to upload image to S3: %v", err)
+	}
+
+	return nil
 }
 
 func main() {
